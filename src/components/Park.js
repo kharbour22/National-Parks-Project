ee5607ff3d10ac3bb54d2445d857c077f41fc93f
KevinHarbour <kharbour22@gmail.com>
2024-03-04 13:26:31 -0500

    Added more elements to homepage

--- src/components/Park.js
+++ src/components/Park.js
@@ -2,8 +2,10 @@ function Park({park}){
     return(
         <li>
             <h2><u>{park.name}</u></h2>
-            <img src={park.image}/>
             <h5>{park.location}</h5>
+            <h4>{park.description}</h4>
+            <h5>Explore: {park.activities}</h5>
+            <img src={park.image}/>
            
         </li>
     )
