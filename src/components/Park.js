a5f6d6dc069bfa5f9e708809c4627b81fb567dbb
KevinHarbour <kharbour22@gmail.com>
2024-03-06 15:08:08 -0500

    Modifyed the layout so that more parks appear on page together

--- src/components/Park.js
+++ src/components/Park.js
@@ -16,10 +16,11 @@ function Park({ park }) {
 
     return (
         <li>
+            <div className="park-container">
             <h2><u>{park.name}</u></h2>
             <h5>{park.location}</h5>
-            <div className="park-container">
-                <h4>{park.description}</h4>
+            
+                <h5>{park.description}</h5>
                 <h5><u>Explore</u>: {park.activities}</h5>
                 <img className="park-image" src={park.image}  alt={park.name} />
                 <br />
--- src/components/ParkList.js
+++ src/components/ParkList.js
@@ -12,7 +12,7 @@ function ParkList(){
       })
     
     return(
-      <><div><p>Have you always been interested in traveling our nation's national parks but don't know where to start? National Parks Explorer provides suggestions for where to start based off your current location or favorite activities. You can mark off completed adventures and even add some more national parks to explore!</p></div><ul>{parkComponents}</ul></>
+      <><div><p>Have you always been interested in traveling our nation's national parks but don't know where to start? National Parks Explorer provides suggestions for where to start based off your current location or favorite activities. You can mark off completed adventures and even add some more national parks to explore!</p></div><ul className="park-list">{parkComponents}</ul></>
     )
 }
 export default ParkList
